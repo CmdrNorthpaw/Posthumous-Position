8b027fa7963bfb3ed24d8cbe1967ef2c696d0220
CmdrNorthpaw <thetuxkid@gmail.com>
2020-04-06 13:36:45 +0100

    The plugin is now finished. Woot.

--- src/main/java/cmdrnorthpaw/posthumousposition/listeners/deathListener.java
+++ src/main/java/cmdrnorthpaw/posthumousposition/listeners/deathListener.java
@@ -1,8 +1,15 @@
 package cmdrnorthpaw.posthumousposition.listeners;
 
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.PlayerDeathEvent;
 
 public class deathListener implements Listener {
-
-
+    @EventHandler
+    public void onPlayerDeath(PlayerDeathEvent event) {
+        int x = event.getEntity().getLocation().getBlockX();
+        int y = event.getEntity().getLocation().getBlockY();
+        int z = event.getEntity().getLocation().getBlockZ();
+        event.getEntity().sendMessage("You died at x:" + x + " y:" + y + " z:"+ z);
+    }
 }
