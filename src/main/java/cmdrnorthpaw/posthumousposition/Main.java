d0c3cd95cf82dad24828df3cfa790b837c560266
CmdrNorthpaw <thetuxkid@gmail.com>
2020-04-06 14:52:39 +0100

    Registered commands in Main.java

--- src/main/java/cmdrnorthpaw/posthumousposition/Main.java
+++ src/main/java/cmdrnorthpaw/posthumousposition/Main.java
@@ -1,5 +1,7 @@
 package cmdrnorthpaw.posthumousposition;
 
+import cmdrnorthpaw.posthumousposition.commands.commandLocateDeath;
+import cmdrnorthpaw.posthumousposition.commands.commandToggleMsg;
 import cmdrnorthpaw.posthumousposition.listeners.deathListener;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -8,6 +10,8 @@ public final class Main extends JavaPlugin {
     @Override
     public void onEnable() {
         getServer().getPluginManager().registerEvents(new deathListener(), this);
+        this.getCommand("toggleDeathMsg").setExecutor(new commandToggleMsg());
+        this.getCommand("locateDeath").setExecutor(new commandLocateDeath());
 
     }
 
