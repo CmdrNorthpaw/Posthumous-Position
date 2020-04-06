2b1478dbbeb76f781eac39c98c1affa846acf6df
CmdrNorthpaw <thetuxkid@gmail.com>
2020-04-06 14:17:40 +0100

    Added a command to toggle the death message

---
+++ src/main/java/cmdrnorthpaw/posthumousposition/commands/commandToggleMsg.java
@@ -0,0 +1,33 @@
+package cmdrnorthpaw.posthumousposition.commands;
+
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class commandToggleMsg implements CommandExecutor {
+    Set<String> noMsgSet = new HashSet<>();
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (sender instanceof Player) {
+            Player player = (Player) sender;
+            if (!noMsgSet.contains(player.getName())) {
+                noMsgSet.add(player.getName());
+                player.sendMessage(ChatColor.RED + "You have disabled death messages");
+            }
+            else {
+                noMsgSet.remove(player.getName());
+                player.sendMessage(ChatColor.GREEN + "Death messages have been enabled");
+            }
+
+
+        }
+
+        return false;
+    }
+}
